4759107fdeb8d60402fa4680c71367a01190c429
lorenzgage <lorenzgage@gmail.com>
2022-08-27 12:16:32 -0500

    adding func

--- ami.py
+++ ami.py
@@ -1,56 +1,18 @@
 from time import*
 from os import*
-num = 0
-while True:    
+def anim(val):
     for x in range(5):
-        if x == (num + 2):
+        if x == (val + 2):
             print('00000  00000')
-        elif x == (num + 1):
+        elif x == (val + 1):
             print('00 00  00 00')
-        elif x == (num + 3):
+        elif x == (val + 3):
             print('0000 00 0000')
 
         else:   
             print('000000000000')
-    sleep(0.25)
+    sleep(0.33)
     system('cls')
-    num += 1    
-    for x in range(5):
-        if x == (num + 2):
-            print('00000  00000')
-        elif x == (num + 1):
-            print('00 00  00 00')
-        elif x == (num + 3):
-            print('0000 00 0000')
-
-        else:   
-            print('000000000000')
-    sleep(0.25)
-    system('cls')
-    num -= 1
-    for x in range(5):
-        if x == (num + 2):
-            print('00000  00000')
-        elif x == (num + 1):
-            print('00 00  00 00')
-        elif x == (num + 3):
-            print('0000 00 0000')
-
-        else:   
-            print('000000000000')
-    sleep(0.25)
-    system('cls')
-    num -= 1    
-    for x in range(5):
-        if x == (num + 2):
-            print('00000  00000')
-        elif x == (num + 1):
-            print('00 00  00 00')
-        elif x == (num + 3):
-            print('0000 00 0000')
-
-        else:   
-            print('000000000000')
-    sleep(0.25)
-    system('cls')
-    num += 1   
+while True:
+    anim(0)
+    anim(1)
