2a993b6bbfb267b588bb03167c2b68fe4f2ffdb3
lorenzgage <lorenzgage@gmail.com>
2022-09-17 15:58:38 -0500

    added stuff

--- astromatix/money/test2.py
+++ astromatix/money/test2.py
@@ -1,3 +1,8 @@
-lists = []
-lists.append(range(10))
-print(lists)
+nx = '0'
+ny = '0'
+lis = ('screan = {')
+for x in range(10):
+    for y in range(10):
+        lis += '\'' + str(x) + str(y) + '\': \'0\', '
+print(lis)
+
